ccd56848ce8fd97c6863b8aa63b1ff2107338709
EC2 Default User <ec2-user@ip-172-31-45-7.ap-northeast-1.compute.internal>
2022-06-09 08:40:31 +0000

    [Add]削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post '/lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/index'
-
-
+  # get 'lists/new'
+  # post '/lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # get 'lists/index'
   get '/top' => 'homes#top'
-  get 'lists/:id'=>'lists#show',as:'list'
-  get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
-  patch 'lists/:id' => 'lists#update', as:'update_list'
-
+  # get 'lists/:id'=>'lists#show',as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as:'destroy_list'
+  resources :lists
+ 
 end
--- db/migrate/20220607081308_create_lists.rb
+++ db/migrate/20220607081308_create_lists.rb
@@ -1,8 +1,7 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-
-      t.timestamps
+　　　t.timestamps
       t.string :title
       t.string :body
     end
